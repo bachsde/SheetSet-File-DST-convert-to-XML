308d900952d2f5887452c6ba3001a55a52af353a
bachsde <86336063+bachsde@users.noreply.github.com>
2025-06-25 17:29:12 +0700

    250625

--- SheetSet_File_DST_Converter/Form1.Designer.cs
+++ SheetSet_File_DST_Converter/Form1.Designer.cs
@@ -28,153 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnGenerateDictionary = new System.Windows.Forms.Button();
-            this.btnDstToXml = new System.Windows.Forms.Button();
-            this.btnBrowseDict = new System.Windows.Forms.Button();
-            this.btnBrowseXml = new System.Windows.Forms.Button();
-            this.btnBrowseDst = new System.Windows.Forms.Button();
-            this.txtDict = new System.Windows.Forms.TextBox();
-            this.txtXml = new System.Windows.Forms.TextBox();
-            this.txtDst = new System.Windows.Forms.TextBox();
-            this.btnXmlToDst = new System.Windows.Forms.Button();
-            this.btnExportExcel = new System.Windows.Forms.Button();
-            this.treeView1 = new System.Windows.Forms.TreeView();
-            this.btnXmlToTreeView = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            btnGenerateDictionary = new System.Windows.Forms.Button();
+            btnDstToXml = new System.Windows.Forms.Button();
+            btnBrowseDict = new System.Windows.Forms.Button();
+            btnBrowseXml = new System.Windows.Forms.Button();
+            btnBrowseDst = new System.Windows.Forms.Button();
+            txtDict = new System.Windows.Forms.TextBox();
+            txtXml = new System.Windows.Forms.TextBox();
+            txtDst = new System.Windows.Forms.TextBox();
+            btnXmlToDst = new System.Windows.Forms.Button();
+            btnExportExcel = new System.Windows.Forms.Button();
+            treeView1 = new System.Windows.Forms.TreeView();
+            btnXmlToTreeView = new System.Windows.Forms.Button();
+            SuspendLayout();
             // 
             // btnGenerateDictionary
             // 
-            this.btnGenerateDictionary.Location = new System.Drawing.Point(279, 74);
-            this.btnGenerateDictionary.Name = "btnGenerateDictionary";
-            this.btnGenerateDictionary.Size = new System.Drawing.Size(121, 23);
-            this.btnGenerateDictionary.TabIndex = 0;
-            this.btnGenerateDictionary.Text = "Generate Dictionary";
-            this.btnGenerateDictionary.UseVisualStyleBackColor = true;
-            this.btnGenerateDictionary.Click += new System.EventHandler(this.btnGenerateDictionary_Click);
+            btnGenerateDictionary.Location = new System.Drawing.Point(326, 85);
+            btnGenerateDictionary.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            btnGenerateDictionary.Name = "btnGenerateDictionary";
+            btnGenerateDictionary.Size = new System.Drawing.Size(141, 27);
+            btnGenerateDictionary.TabIndex = 0;
+            btnGenerateDictionary.Text = "Generate Dictionary";
+            btnGenerateDictionary.UseVisualStyleBackColor = true;
+            btnGenerateDictionary.Click += btnGenerateDictionary_Click;
             // 
             // btnDstToXml
             // 
-            this.btnDstToXml.Location = new System.Drawing.Point(406, 10);
-            this.btnDstToXml.Name = "btnDstToXml";
-            this.btnDstToXml.Size = new System.Drawing.Size(165, 23);
-            this.btnDstToXml.TabIndex = 1;
-            this.btnDstToXml.Text = "DST To XML";
-            this.btnDstToXml.UseVisualStyleBackColor = true;
-            this.btnDstToXml.Click += new System.EventHandler(this.btnDstToXml_Click);
+            btnDstToXml.Location = new System.Drawing.Point(474, 12);
+            btnDstToXml.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            btnDstToXml.Name = "btnDstToXml";
+            btnDstToXml.Size = new System.Drawing.Size(192, 27);
+            btnDstToXml.TabIndex = 1;
+            btnDstToXml.Text = "DST To XML";
+            btnDstToXml.UseVisualStyleBackColor = true;
+            btnDstToXml.Click += btnDstToXml_Click;
             // 
             // btnBrowseDict
             // 
-            this.btnBrowseDict.Location = new System.Drawing.Point(279, 106);
-            this.btnBrowseDict.Name = "btnBrowseDict";
-            this.btnBrowseDict.Size = new System.Drawing.Size(121, 23);
-            this.btnBrowseDict.TabIndex = 13;
-            this.btnBrowseDict.Text = "Browse Dictionary";
-            this.btnBrowseDict.UseVisualStyleBackColor = true;
-            this.btnBrowseDict.Click += new System.EventHandler(this.btnBrowseDict_Click_1);
+            btnBrowseDict.Location = new System.Drawing.Point(326, 122);
+            btnBrowseDict.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            btnBrowseDict.Name = "btnBrowseDict";
+            btnBrowseDict.Size = new System.Drawing.Size(141, 27);
+            btnBrowseDict.TabIndex = 13;
+            btnBrowseDict.Text = "Browse Dictionary";
+            btnBrowseDict.UseVisualStyleBackColor = true;
+            btnBrowseDict.Click += btnBrowseDict_Click_1;
             // 
             // btnBrowseXml
             // 
-            this.btnBrowseXml.Location = new System.Drawing.Point(279, 42);
-            this.btnBrowseXml.Name = "btnBrowseXml";
-            this.btnBrowseXml.Size = new System.Drawing.Size(121, 23);
-            this.btnBrowseXml.TabIndex = 12;
-            this.btnBrowseXml.Text = "Browse .XML";
-            this.btnBrowseXml.UseVisualStyleBackColor = true;
-            this.btnBrowseXml.Click += new System.EventHandler(this.btnBrowseXml_Click_1);
+            btnBrowseXml.Location = new System.Drawing.Point(326, 48);
+            btnBrowseXml.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            btnBrowseXml.Name = "btnBrowseXml";
+            btnBrowseXml.Size = new System.Drawing.Size(141, 27);
+            btnBrowseXml.TabIndex = 12;
+            btnBrowseXml.Text = "Browse .XML";
+            btnBrowseXml.UseVisualStyleBackColor = true;
+            btnBrowseXml.Click += btnBrowseXml_Click_1;
             // 
             // btnBrowseDst
             // 
-            this.btnBrowseDst.Location = new System.Drawing.Point(279, 10);
-            this.btnBrowseDst.Name = "btnBrowseDst";
-            this.btnBrowseDst.Size = new System.Drawing.Size(121, 23);
-            this.btnBrowseDst.TabIndex = 11;
-            this.btnBrowseDst.Text = "Browse .DST";
-            this.btnBrowseDst.UseVisualStyleBackColor = true;
-            this.btnBrowseDst.Click += new System.EventHandler(this.btnBrowseDst_Click_1);
+            btnBrowseDst.Location = new System.Drawing.Point(326, 12);
+            btnBrowseDst.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            btnBrowseDst.Name = "btnBrowseDst";
+            btnBrowseDst.Size = new System.Drawing.Size(141, 27);
+            btnBrowseDst.TabIndex = 11;
+            btnBrowseDst.Text = "Browse .DST";
+            btnBrowseDst.UseVisualStyleBackColor = true;
+            btnBrowseDst.Click += btnBrowseDst_Click_1;
             // 
             // txtDict
             // 
-            this.txtDict.Location = new System.Drawing.Point(12, 108);
-            this.txtDict.Name = "txtDict";
-            this.txtDict.Size = new System.Drawing.Size(261, 20);
-            this.txtDict.TabIndex = 10;
+            txtDict.Location = new System.Drawing.Point(14, 125);
+            txtDict.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            txtDict.Name = "txtDict";
+            txtDict.Size = new System.Drawing.Size(304, 23);
+            txtDict.TabIndex = 10;
             // 
             // txtXml
             // 
-            this.txtXml.Location = new System.Drawing.Point(12, 44);
-            this.txtXml.Name = "txtXml";
-            this.txtXml.Size = new System.Drawing.Size(261, 20);
-            this.txtXml.TabIndex = 9;
+            txtXml.Location = new System.Drawing.Point(14, 51);
+            txtXml.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            txtXml.Name = "txtXml";
+            txtXml.Size = new System.Drawing.Size(304, 23);
+            txtXml.TabIndex = 9;
             // 
             // txtDst
             // 
-            this.txtDst.Location = new System.Drawing.Point(12, 12);
-            this.txtDst.Name = "txtDst";
-            this.txtDst.Size = new System.Drawing.Size(261, 20);
-            this.txtDst.TabIndex = 8;
+            txtDst.Location = new System.Drawing.Point(14, 14);
+            txtDst.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            txtDst.Name = "txtDst";
+            txtDst.Size = new System.Drawing.Size(304, 23);
+            txtDst.TabIndex = 8;
             // 
             // btnXmlToDst
             // 
-            this.btnXmlToDst.Location = new System.Drawing.Point(406, 42);
-            this.btnXmlToDst.Name = "btnXmlToDst";
-            this.btnXmlToDst.Size = new System.Drawing.Size(165, 23);
-            this.btnXmlToDst.TabIndex = 14;
-            this.btnXmlToDst.Text = "XML To DST";
-            this.btnXmlToDst.UseVisualStyleBackColor = true;
-            this.btnXmlToDst.Click += new System.EventHandler(this.btnXmlToDst_Click);
+            btnXmlToDst.Location = new System.Drawing.Point(474, 48);
+            btnXmlToDst.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            btnXmlToDst.Name = "btnXmlToDst";
+            btnXmlToDst.Size = new System.Drawing.Size(192, 27);
+            btnXmlToDst.TabIndex = 14;
+            btnXmlToDst.Text = "XML To DST";
+            btnXmlToDst.UseVisualStyleBackColor = true;
+            btnXmlToDst.Click += btnXmlToDst_Click;
             // 
             // btnExportExcel
             // 
-            this.btnExportExcel.Location = new System.Drawing.Point(406, 106);
-            this.btnExportExcel.Name = "btnExportExcel";
-            this.btnExportExcel.Size = new System.Drawing.Size(165, 23);
-            this.btnExportExcel.TabIndex = 15;
-            this.btnExportExcel.Text = "Export XML To Excel";
-            this.btnExportExcel.UseVisualStyleBackColor = true;
-            this.btnExportExcel.Click += new System.EventHandler(this.btnExportExcel_Click);
+            btnExportExcel.Location = new System.Drawing.Point(474, 122);
+            btnExportExcel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            btnExportExcel.Name = "btnExportExcel";
+            btnExportExcel.Size = new System.Drawing.Size(192, 27);
+            btnExportExcel.TabIndex = 15;
+            btnExportExcel.Text = "Export XML To Excel";
+            btnExportExcel.UseVisualStyleBackColor = true;
+            btnExportExcel.Click += btnExportExcel_Click;
             // 
             // treeView1
             // 
-            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.treeView1.Location = new System.Drawing.Point(12, 137);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(559, 172);
-            this.treeView1.TabIndex = 16;
+            treeView1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            treeView1.Location = new System.Drawing.Point(14, 158);
+            treeView1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            treeView1.Name = "treeView1";
+            treeView1.Size = new System.Drawing.Size(652, 198);
+            treeView1.TabIndex = 16;
             // 
             // btnXmlToTreeView
             // 
-            this.btnXmlToTreeView.Location = new System.Drawing.Point(406, 74);
-            this.btnXmlToTreeView.Name = "btnXmlToTreeView";
-            this.btnXmlToTreeView.Size = new System.Drawing.Size(165, 23);
-            this.btnXmlToTreeView.TabIndex = 17;
-            this.btnXmlToTreeView.Text = "Read XML To TreeView";
-            this.btnXmlToTreeView.UseVisualStyleBackColor = true;
-            this.btnXmlToTreeView.Click += new System.EventHandler(this.btnXmlToTreeView_Click);
+            btnXmlToTreeView.Location = new System.Drawing.Point(474, 85);
+            btnXmlToTreeView.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            btnXmlToTreeView.Name = "btnXmlToTreeView";
+            btnXmlToTreeView.Size = new System.Drawing.Size(192, 27);
+            btnXmlToTreeView.TabIndex = 17;
+            btnXmlToTreeView.Text = "Read XML To TreeView";
+            btnXmlToTreeView.UseVisualStyleBackColor = true;
+            btnXmlToTreeView.Click += btnXmlToTreeView_Click;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(583, 321);
-            this.Controls.Add(this.btnXmlToTreeView);
-            this.Controls.Add(this.treeView1);
-            this.Controls.Add(this.btnExportExcel);
-            this.Controls.Add(this.btnXmlToDst);
-            this.Controls.Add(this.btnBrowseDict);
-            this.Controls.Add(this.btnBrowseXml);
-            this.Controls.Add(this.btnBrowseDst);
-            this.Controls.Add(this.txtDict);
-            this.Controls.Add(this.txtXml);
-            this.Controls.Add(this.txtDst);
-            this.Controls.Add(this.btnDstToXml);
-            this.Controls.Add(this.btnGenerateDictionary);
-            this.MinimumSize = new System.Drawing.Size(599, 360);
-            this.Name = "Form1";
-            this.Text = "SheetSet File .DST Converter";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(680, 370);
+            Controls.Add(btnXmlToTreeView);
+            Controls.Add(treeView1);
+            Controls.Add(btnExportExcel);
+            Controls.Add(btnXmlToDst);
+            Controls.Add(btnBrowseDict);
+            Controls.Add(btnBrowseXml);
+            Controls.Add(btnBrowseDst);
+            Controls.Add(txtDict);
+            Controls.Add(txtXml);
+            Controls.Add(txtDst);
+            Controls.Add(btnDstToXml);
+            Controls.Add(btnGenerateDictionary);
+            Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            MinimumSize = new System.Drawing.Size(696, 409);
+            Name = "Form1";
+            Text = "SheetSet File .DST Converter - Dev by Ba'ch Nguyen";
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
